11bf0716c6d11f616445596cecf2ef21cc1ad80b
flybywind@foxmail.com <flybywind@foxmail.com>
2016-08-09 15:39:45 +0800

    增加message box功能， doing；增加socket router， doing

--- chat-socket/chat_socket.go
+++ chat-socket/chat_socket.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -18,46 +19,54 @@ type Msg struct {
 	Content,
 	RoomName,
 	Token,
-	Type string
+	Type,
+	EventName string
 }
 
 const (
-	OwnMsg              = "0"
-	NormalMsg           = "1"
-	EventMsg            = "9"
+	OwnMsg    = "0"
+	NormalMsg = "1"
+	EventMsg  = "9"
+)
+
+type chatId string
+type roomId string
+
+var (
+	WriteWaitSec        = 10 * time.Second
+	rooms               = make(map[roomId]*roomSocket)
 	RoomNotExists       = func(room string) error { return fmt.Errorf("room [%s] not exists", room) }
 	JoinRoomWithoutAuth = func(room string) error { return fmt.Errorf("invalid to join room: %s", room) }
 )
 
-var (
-	WriteWaitSec = 10 * time.Second
-	rooms        = make(map[string]*RoomSocket)
-)
-
-type RoomSocket struct {
-	name, token string
-	members     []*ChatConn
+type roomSocket struct {
+	token   string
+	name    roomId
+	members map[roomId]*ChatConn
+	msgbox  map[chatId]time.Time
 }
 type ChatConn struct {
-	con        *websocket.Conn
-	name       string
-	belongRoom string
-	ownRoom    bool
+	con            *websocket.Conn
+	name           chatId
+	belongRoom     roomId
+	ownRoom        bool
+	leaveRoom      bool
+	event_handlers map[string]func(*ChatConn, *Msg)
 }
 
-func createRoom(name, token string) (*RoomSocket, error) {
+func createRoom(name roomId, token string) (*roomSocket, error) {
 	room, exists := rooms[name]
 	if exists {
 		return nil, fmt.Errorf("room [%s] exists", name)
 	}
-	room = &RoomSocket{
-		name, make([]*ChatConn), token,
+	room = &roomSocket{
+		name, token, []*ChatConn{},
 	}
 
 	rooms[name] = room
 	return room, nil
 }
-func findRoom(name string) *RoomSocket {
+func findRoom(name roomId) *roomSocket {
 	room, exists := rooms[name]
 	if exists {
 		return room
@@ -66,53 +75,72 @@ func findRoom(name string) *RoomSocket {
 	}
 }
 
-func (r *RoomSocket) BroadCast(fromUser, string, msg Msg) error {
-
+func (r *roomSocket) broadCast(fromChat chatId, msg Msg) error {
 	buf := bytes.NewBuffer(nil)
 	if err := json.NewEncoder(buf).Encode(msg); err == nil {
 		for _, conn := range r.members {
-			if conn.Name() == fromUser {
-				msg.Type = OwnMsg
-				buf2 := bytes.NewBuffer(make([]byte, buf.Len()))
-				err = json.NewEncoder(buf2).Encode(msg)
-				if err != nil {
-					return err
+			if !conn.leaveRoom {
+				if conn.Name() == fromChat {
+					msg.Type = OwnMsg
+					buf2 := bytes.NewBuffer(make([]byte, buf.Len()))
+					err = json.NewEncoder(buf2).Encode(msg)
+					if err != nil {
+						return err
+					}
+					return conn.con.WriteMessage(websocket.TextMessage, buf2.Bytes())
+				} else {
+					return conn.con.WriteMessage(websocket.TextMessage, buf.Bytes())
 				}
-				return conn.con.WriteMessage(websocket.TextMessage, buf2.Bytes())
 			} else {
-				return conn.con.WriteMessage(websocket.TextMessage, buf.Bytes())
+				now := time.Now()
+				if _, exists := r.msgbox[conn.name]; !exists {
+					r.msgbox[conn.name] = now
+				}
+				// TODO: get db to store messages
 			}
 		}
 	} else {
 		return err
 	}
+	return nil
 }
 
-func (r *RoomSocket) AddConn(c *ChatConn) {
-	r.members = append(r.members, c)
+func (r *roomSocket) addConn(c *ChatConn) {
+	if conn, existsbefore := r.members[c.name]; existsbefore {
+		c.ownRoom = conn.belongRoom
+		// TODO: retreive older messages in msgbox
+		delete(r.msgbox, c.name)
+	}
+
+	r.members[c.name] = c
 }
 
-func (c *ChatConn) Name() string {
+func (c *ChatConn) Name() chatId {
 	return c.name
 }
 func (c *ChatConn) BroadCast(msg Msg) error {
-	roomSocket := findRoom(belongRoom)
+	roomSocket := findRoom(c.belongRoom)
 
 	if roomSocket == nil {
-		return RoomNotExists(belongRoom)
+		return RoomNotExists(c.belongRoom)
 	}
 
-	roomSocket.BroadCast(c.name, msg)
+	roomSocket.broadCast(c.name, msg)
+	return nil
 }
 func (c *ChatConn) Active() {
 	c.con.SetReadDeadline(time.Time{})
 }
-func (c *ChatConn) joinRoom(room string, token string) error {
+
+func (c *ChatConn) SetName(name chatId) {
+	c.name = name
+}
+func (c *ChatConn) JoinRoom(room roomId, token string) error {
 	roomSocket := findRoom(room)
 
 	if roomSocket != nil {
 		if roomSocket.token == token {
-			roomSocket.AddConn(c)
+			roomSocket.addConn(c)
 			c.belongRoom = room
 		} else {
 			// TODO: need owner allowed
@@ -121,13 +149,14 @@ func (c *ChatConn) joinRoom(room string, token string) error {
 		return nil
 	} else {
 		if roomSocket, err := createRoom(room, token); err == nil {
-			roomSocket.AddConn(c)
+			roomSocket.addConn(c)
 			c.belongRoom = room
 			c.ownRoom = true
 		} else {
 			return err
 		}
 	}
+	return nil
 }
 
 func (c *ChatConn) waitMessage() {
@@ -141,18 +170,13 @@ func (c *ChatConn) waitMessage() {
 			case NormalMsg:
 				c.BroadCast(msg)
 			case EventMsg:
-				cmd := strings.ToLower(msg.Content)
-				switch cmd {
-				case "join":
-					err = c.joinRoom(msg.RoomName, msg.Token)
-				case "leave":
-					c.con.Close()
-				default:
-					err = fmt.Errorf("not recognizied action")
-				}
-				if err == nil {
-					msg.Content = "0"
+				cmd := strings.ToLower(msg.EventName)
+				if h, ok := c.event_handlers[cmd]; !ok {
+					h(c, &msg)
 				} else {
+					err = fmt.Errorf("event: %s not registered", cmd)
+				}
+				if err != nil {
 					msg.Content = err.Error()
 				}
 				c.Emit(msg)
@@ -160,11 +184,48 @@ func (c *ChatConn) waitMessage() {
 		}
 	}
 }
+func (c *ChatConn) On(event_name string, handler func(*ChatConn, *Msg)) {
+	event_name = strings.ToLower(event_name)
+	c.event_handlers[event_name] = handler
+}
 
-func (c *ChatConn) Emit(msg Msg) {
+func (c *ChatConn) Emit(msg Msg) error {
 	if msg.Type == EventMsg {
-		c.BroadCast(msg)
+		buf := bytes.NewBuffer(nil)
+		if err := json.NewEncoder(buf).Encode(msg); err == nil {
+			return c.con.WriteMessage(websocket.TextMessage, buf.Bytes())
+		} else {
+			return err
+		}
 	} else {
 		panic("should emit event actions")
 	}
+}
+
+func (c *ChatConn) LeaveRoom() {
+	c.leaveRoom = true
+	c.event_handlers = nil
+}
+func (c *ChatConn) Close() {
+	c.LeaveRoom()
+	c.con.Close()
+}
+
+var upgrader = websocket.Upgrader{}
+
+func NewChatConn(w http.ResponseWriter, r *http.Request) (chat *ChatConn, e error) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+	name := ws.Subprotocol()
+	chat = &ChatConn{ws, name, "", false, false, map[string]func(*ChatConn, *Msg){}}
+	chat.Active()
+	chat.Emit(Msg{
+		Type:      EventMsg,
+		EventName: "connect",
+	})
+	go chat.waitMessage()
+
+	return chat, nil
 }
--- socketio_server.go
+++ socketio_server.go
@@ -24,44 +24,13 @@ func main() {
 	r := router.GetRouter()
 	r.AddRouterMethod("/", "get", handler.Index)
 	r.AddRouterMethod("/chat_room/:room.:user", "get", handler.Rooms)
+	r.AddRouterMethod("/chat_room/:room.:user?ws", "get", handler.Chat)
 	r.AddStatic("static/javascripts")
 	r.AddStatic("static/stylesheets")
-	//r.AddStatic("static/javascripts/socket.io")
 	server := http.Server{
 		Addr:    listenAddr,
 		Handler: router.Entry{},
 	}
-
-	socket.On("connection", func(so socketio.Socket) {
-		log.Println("server connection from client!") // 收到！
-		ns := socket.Of("/chat_room")
-		so.On("join_room", func(data string) {
-			log.Println("server join_room!") // 没有收到
-			so.Emit("user_entered", data)
-		})
-		so.On("message", func(message string) {
-			log.Println("server receive message:", message)
-		})
-		ns.On("join_room", func(data string) {
-			log.Println("ns receive join room request:", data)
-			so.Emit("user_entered", data)
-		})
-		ns.On("message", func(message string) {
-			log.Println("ns receive message:", message)
-		})
-
-	})
-
-	/*ns.On("connection", func(so socketio.Socket) {
-		log.Println("namespace connection from client!") // 没有收到
-		so.On("join_room", func(data string) {
-			log.Println("receive join room request:", data)
-			so.Emit("user_entered", data)
-		})
-		so.On("message", func(message string) {
-			log.Println("receive message:", message)
-		})
-	})*/
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalln(err)
